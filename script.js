6a0f55580bea70f71c7c932872c3a97fa506d760
mariaLuizaZT_whataru <maria.zivigicoski.teixeira@escola.pr.gov.br>
2026-07-06 11:52:51 -0300

    botao funcionou

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document. getElementById("curtidas")
+botao.addEventListener("click",aumentarCurtidas) 
+
+function aumentarCurtidas(){
+let curtida = document.querySelector("span")
+curtida.textContent
+}
